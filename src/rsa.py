71b250458fef35909d9acc80133591e113dc8de5
Nam Phuong Do <87682531+dpnam2112@users.noreply.github.com>
2024-11-11 00:19:09 +0700

    tests: add testcases for encryption and decryption (#9)

--- src/rsa.py
+++ src/rsa.py
@@ -1,11 +1,12 @@
+from typing import Callable
 from .prime_generator import PrimeGenerator
 from .rsa_key_generator import generate_encryption_key, generate_decryption_key
-from .modular_exp import modular_exp
 
 class RSA:
     def __init__(
         self,
-        prime_generator: PrimeGenerator
+        prime_generator: PrimeGenerator,
+        modular_exp: Callable
     ):
         """Initializes the RSA instance with a prime generator and generates the RSA keys.
 
@@ -16,6 +17,7 @@ class RSA:
         p = self.prime_generator()
         q = self.prime_generator()
         self.public_key, self.private_key = self._generate_keys(p, q)
+        self.modular_exp = modular_exp
 
 
     def _generate_keys(self, p, q):
@@ -41,7 +43,7 @@ class RSA:
         """
         n, e = self.public_key
         # Encrypt using modular exponentiation
-        encrypted_message = modular_exp(message, e, n)
+        encrypted_message = self.modular_exp(message, e, n)
         return encrypted_message
 
     def decrypt(self, encrypted_message: int) -> int:
@@ -55,5 +57,5 @@ class RSA:
         """
         n, d = self.private_key
         # Decrypt using modular exponentiation
-        decrypted_message = modular_exp(encrypted_message, d, n)
+        decrypted_message = self.modular_exp(encrypted_message, d, n)
         return decrypted_message
